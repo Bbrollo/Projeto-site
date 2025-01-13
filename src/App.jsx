b0010929456de269e8825c6e371302fe12582c02
Breno Brollo <brenobrolloo@gmail.com>
2025-01-13 11:35:51 -0300

    Iniciando o cabecalho

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+import logo from './logo.svg';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
