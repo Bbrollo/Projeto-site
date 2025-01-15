890a006ed57d2e08b4cd941f8c145edab3443624
Breno Brollo <brenobrolloo@gmail.com>
2025-01-15 15:41:08 -0300

    Banner de navegação ajustado

--- src/components/BannerNavegacao/BannerNavegacao.jsx
+++ src/components/BannerNavegacao/BannerNavegacao.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import './BannerNavegacao.scss';
+import useIsMobile from '../../isMobile';
 
 const BannerNavegacao = () => {
+
+    const isMobile = useIsMobile();
+
     return(
-        <div className="container-img-flex">
-            <img src=".\static\images\banner-grande-nav.png" alt="Banner de Navegação Grande" className="imagem-nav"/>
-            <img src="\static\images\banner-pequeno-nav.png" alt="Banner de Navegação Pequeno"  className="imagem-nav"/>
+        <div>
+            {isMobile ? (
+                <div className="container-img-flex-mobile">
+                    <img src="\static\images\banner-pequeno-nav-mobile.png" alt="Banner de Navegação Pequeno" className="img-nav-mobile"/>
+                    <img src="\static\images\banner-grande-nav-mobile.png" alt="Banner de Navegação Grande" className="img-nav-mobile"/>
+                </div>
+            ) : (
+                <div className="container-img-flex">
+                <img src=".\static\images\banner-grande-nav.png" alt="Banner de Navegação Grande" className="imagem-nav-1"/>
+                <img src="\static\images\banner-pequeno-nav.png" alt="Banner de Navegação Pequeno"  className="imagem-nav-2"/>
+            </div>
+            )}
         </div>
     )
 
