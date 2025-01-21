7ffb7aec69905e0911d82e281e0a03a0e10f76d2
Breno Brollo <brenobrolloo@gmail.com>
2025-01-21 17:57:20 -0300

    Criando carrinho desktop

--- src/components/Carrinho/Carrinho.jsx
+++ src/components/Carrinho/Carrinho.jsx
@@ -64,7 +64,6 @@ const Carrinho = ({isCarrinhoOpen, toggleCarrinho, carrinho}) => {
                                                 )}
                                             </span> 
                                     </div>
-                                <div className='teste'>content</div>
                             </div>
                         ))
                     ) : (
@@ -80,7 +79,46 @@ const Carrinho = ({isCarrinhoOpen, toggleCarrinho, carrinho}) => {
                 </div>
             </div>
         ) : (
-            <h1>oi</h1>
+            <div className={`carrinho ${isCarrinhoOpen ? "open" : ""}`}>
+                <div className='header-carrinho'>
+                    <span className='titulo-carrinho'>Carrinho</span>
+                    <button className="close-button-carrinho" onClick={toggleCarrinho}>
+                        <img src="\static\images\close-icon.png" alt="Botão fechar Menu" /> {/* Botão para fechar */}           
+                    </button>
+                </div>
+                <div className='container-produtos-carrinho'>
+                    {carrinho.length > 0 ? (
+                        carrinho.map((produto, index) => (
+                            <div key={index} className='produtp-itens'>
+                                <img src={produto.image} alt="Imagem produto" className='img-prod-carrinho'/>
+                                <div className='nome-tamanho-preco'>
+                                        <div className='nome-tamanho'>
+                                            <span className='prod-name-carrinho'>{produto.nome}</span>
+                                            <span className='prod-tamanho-carrinho'>Tamanho: {produto.tamanho}</span>
+                                            </div>
+                                            <span className='prod-preco-carrinho'>
+                                                Preço: R$
+                                                {produto.desconto !== null ? (
+                                                    `${produto.desconto}`
+                                                ) : (
+                                                    `${produto.preco}`
+                                                )}
+                                            </span> 
+                                    </div>
+                            </div>
+                        ))
+                    ) : (
+                        <p>O carrinho está vazio</p>
+                    )}        
+                </div>
+                <div className='container-total-carrinho'>
+                        <div className='textos-total-carrinho'><span>SubTotal: </span><span>{somaTotalSemDesconto()}</span></div>
+                        <div className='textos-total-carrinho'><span>Descontos: </span><span>- {somaTotalDescontos()}</span></div>
+                        <div className='textos-total-carrinho'><span>Total:</span><span>{total()}</span></div>
+                        <button className='btn-finalizar-pedido'>Finalizar Pedido</button>
+                        <a className='continuar-comprando' onClick={toggleCarrinho}>Continuar Comprando</a>
+                </div>
+            </div>
         )}
         </>
     );
